ec94a2fc91b9f4e9ebecec79ecae556e18123ca9
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-07-23 10:20:30 -0700

    inquirer

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const fs = require("fs");
+// const fs = require("fs");
+const inquirer = require("inquirer");
 
-const generatePage = require("./src/page-template.js");
+// const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(portfolioData);
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2); // kept for reference
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+// const [name, github] = profileDataArgs; //array destructuring statement, kept for reference
 
-  console.log(
-    "Portfolio complete! Check out the index.html to see the output!"
-  );
-});
+// fs.writeFile("./index.html", generatePage(name, github), (err) => {
+//   if (err) throw new Error(err);
+
+//   console.log(
+//     "Portfolio complete! Check out the index.html to see the output!"
+//   );
+// });
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
